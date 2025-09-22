96b72118844da4fcf2bfdcf58bdfebdbffc31ab4
Benkolon152 <v.benasz@gmail.com>
2025-09-22 08:45:24 +0200

    Get sql

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -1,9 +1,38 @@
 const express = require('express');
+const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+})
+
+
+
+app.get('/musli', (req, res) =>{
+    //conn.connect(err => console.warn('connect err', err))
+    //console.log('conn', conn)
+
+    conn.query("SELECT id, name, price From musli", (err, result, fields) => {
+        if (err){
+            console.warn(err)
+        }
+        if (result){
+            console.log(result)
+            console.log(fields)
+
+            res.status(200).json({err, result, fields})
+        }else{
+            res.status(403).json({err,})
+        }
+    })
+})
 
 app.get((req, res) => {
     if (err){
