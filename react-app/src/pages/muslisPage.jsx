1ee87ee1b7f6de499dd456a4435714a48bb50364
Benkolon152 <v.benasz@gmail.com>
2025-09-23 12:36:47 +0200

    css

--- react-app/src/pages/muslisPage.jsx
+++ react-app/src/pages/muslisPage.jsx
@@ -13,23 +13,25 @@ export default class MuslisPage extends React.Component{
             <div>
             <main id="content-area">
               <h1>My mueslis</h1>
-              <table>
-                <thead>
-                  <tr>
-                    <th>id</th>
-                    <th>name</th>
-                    <th>price</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {/*JSON.stringify(this.state.musliData)*/}
-                  {this.state.musliData.result.map(musli => <tr key={musli.id}>
-                    <td>{musli.id}</td>
-                    <td>{musli.name}</td>
-                    <td>{musli.price}</td>
-                  </tr>)}
-                </tbody>
-              </table>
+              <div>
+                <table className="table-wrapper">
+                  <thead>
+                    <tr>
+                      <th>id</th>
+                      <th>name</th>
+                      <th>price</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/*JSON.stringify(this.state.musliData)*/}
+                    {this.state.musliData.result.map(musli => <tr key={musli.id}>
+                      <td>{musli.id}</td>
+                      <td>{musli.name}</td>
+                      <td>{musli.price}</td>
+                    </tr>)}
+                  </tbody>
+                </table>
+              </div>
             </main>
             </div>
         )
